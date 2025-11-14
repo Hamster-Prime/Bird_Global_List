a6411839ce35b9b15ad20e8fe61525de70ba5010
Sanite&Ava <y15383640831@icloud.com>
2025-11-14 12:02:35 +0800

    update

--- extract.py
+++ extract.py
@@ -1,23 +1,19 @@
 import pandas as pd
 import os
 
-# 确保 iplist.csv 在同一目录下
 input_file = 'ipinfo_lite.csv'
 
 if not os.path.exists(input_file):
     print(f"错误: 未找到文件 {input_file}")
 else:
-    # 读取CSV文件
     df = pd.read_csv(input_file)
 
-    # 过滤掉 IPv6 地址 (包含 ':' 的网络地址)
     df_ipv4 = df[~df['network'].str.contains(':')].copy()
     df_ipv6 = df[df['network'].str.contains(':')].copy()
 
     output_dir = 'Country_CIDR'
     os.makedirs(output_dir, exist_ok=True)
 
-    # 按国家代码分组并导出 IPv4 地址
     for cc, group in df_ipv4.groupby('country_code'):
         list_name = f"{cc}"
         filename = os.path.join(output_dir, f"{list_name}.conf")
@@ -25,7 +21,6 @@ else:
         print(f"正在生成 {filename}...")
 
         with open(filename, 'w') as f:
-            # 写入头部
             f.write(f"# {list_name} cidr address-list\n")
             for net in group['network']:
                 if '/' not in str(net):
